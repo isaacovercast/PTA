502004ad1fda00683e56b135174cbc9b8e4b3745
Isaac Overcast <isaac.overcast@gmail.com>
2019-10-03 15:30:48 -0400

    "Updating PTA/__init__.py to version - 0.0.11

--- PTA/__init__.py
+++ PTA/__init__.py
@@ -1,7 +1,7 @@
 """
 PTA - Phylogeographic Temporal Analysis
 """
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 __author__ = "Isaac Overcast"
 
 ## Possible values for __loglevel__: "DEBUG"  "INFO"  "WARN"  "ERROR"
---
+++ docs/releasenotes.rst
@@ -0,0 +1,9 @@
+.. _release_notes:
+
+Release Notes
+=============
+
+0.0.11
+------
+**October 03, 2019**
+

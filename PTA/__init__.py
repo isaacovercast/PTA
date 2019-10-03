54936dae48d17edfa863e9ca3010c792a1a84863
Isaac Overcast <isaac.overcast@gmail.com>
2019-10-03 14:49:43 -0400

    Fixing init

--- PTA/__init__.py
+++ PTA/__init__.py
@@ -1,7 +1,7 @@
 """
 PTA - Phylogeographic Temporal Analysis
 """
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 __author__ = "Isaac Overcast"
 
 ## Possible values for __loglevel__: "DEBUG"  "INFO"  "WARN"  "ERROR"

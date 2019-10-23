af0de763d37392e9320e2fcabeefaab2cbba0138
Isaac Overcast <isaac.overcast@gmail.com>
2019-10-23 13:34:48 -0400

    conda tweaks

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
